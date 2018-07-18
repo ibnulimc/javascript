5d21490c380145bd60052547ee7182da1ae33e83
ibnulimc <ibnulimc@gmail.com>
2018-07-18 15:30:46 +0700

    array dalam object file object.js

--- object.js
+++ object.js
@@ -2,11 +2,13 @@ function panggilObject() {
   var mobil = {
     type: 'sedan',
     harga: 200000000,
-    warna: 'Putih'
+    warna: 'Putih',
+    tahun: [2001,2002,2003,2004]
   }
 
   console.log(mobil)
   console.log(mobil.type)
+  console.log(mobil.tahun[2])
 }
 
 panggilObject()
