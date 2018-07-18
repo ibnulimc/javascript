dc1362461d3972f2f1e87ddea294829ecab8ba57
ibnulimc <ibnulimc@gmail.com>
2018-07-18 15:25:14 +0700

    Pengenalan Object

---
+++ object.js
@@ -0,0 +1,12 @@
+function panggilObject() {
+  var mobil = {
+    type: 'sedan',
+    harga: 200000000,
+    warna: 'Putih'
+  }
+
+  console.log(mobil)
+  console.log(mobil.type)
+}
+
+panggilObject()
