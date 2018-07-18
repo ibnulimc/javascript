d8c22180f7024a0e32b42701b0b11b4a96600005
ibnulimc <ibnulimc@gmail.com>
2018-07-18 15:16:56 +0700

    Nested Array(array di dalam array)

---
+++ nested-array.js
@@ -0,0 +1,13 @@
+function panggilNestedArray() {
+  var datanama = [
+    ["Ab", "Ac", "Ad"],
+    ["bola1", "bola2", "bola3"],
+    ["rem1", "rem2", "rem3"]
+  ]
+
+  console.log(datanama.length)
+  console.log(datanama[1][2])
+
+}
+
+panggilNestedArray()
