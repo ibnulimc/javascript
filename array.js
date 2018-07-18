b65c39d30aa78da754dadf29819879bbdf3338ef
ibnulimc <ibnulimc@gmail.com>
2018-07-18 15:05:15 +0700

    Pengenalan Array

---
+++ array.js
@@ -0,0 +1,9 @@
+function panggilArray() {
+  var data = [1,2,3,4,5,9]
+  var nama = ["abis", "abi2", "abi3"]
+
+  console.log(data.length); //menghitung array dengan .length
+  console.log(nama[2]); //memanggil / menampilkan data pada array
+}
+
+panggilArray();
